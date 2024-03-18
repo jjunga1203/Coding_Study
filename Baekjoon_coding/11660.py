b3e1dff39cf8e28c168bd495b4a197dfd17e2ae5
jjunga1203 <jjunga1203@gmail.com>
2024-03-19 00:09:15 +0900

    homework commit

--- Baekjoon_coding/11660.py
+++ Baekjoon_coding/11660.py
@@ -1,3 +1,5 @@
+# https://www.acmicpc.net/problem/11660
+
 import sys
 
 sys.stdin = open("11660.md", "r")
@@ -5,31 +7,34 @@ sys.stdin = open("11660.md", "r")
 str = input().split(' ')
 N = int(str[0])
 k = int(str[1])
-tmp_arr = []
-arr = []
-sum_rows = []
-sum_cols = []
+
+result = ''
+sum_arr = [[0] * N for _ in range(N)]
 
 # 입력 matrix
-for _ in range(0, N):
-    tmp = [int(x) for x in input().split(' ')]
-    sum_rows.append(sum(tmp))
-    arr.append(tmp)
+for i in range(N):
+    # 한줄씩 읽기 -> 1차원 배열로 받기
+    tmp = list(map(int, input().split(' ')))
+    # print(tmp)
+    for j in range(N):
+        for idx in range(j+1):
+            sum_arr[i][j] += tmp[idx]
 
-print(sum_rows)
+# print(sum_arr)
 
 # 구해야 할 범위 test_case
-test_cases = []
-for _ in range(0, k):
-    test_cases.append([int(x) for x in input().split(' ')])
-
-for test_case in test_cases:
+for m in range(k):
+    test_case = list(map(int, input().split(' ')))
     sum = 0
-    x1, y1 = test_case[0], test_case[1]
-    x2, y2 = test_case[2], test_case[3]
+    x1, y1 = test_case[0]-1, test_case[1]-1
+    x2, y2 = test_case[2]-1, test_case[3]-1
 
-    for i in range(x1-1, x2):
-        for j in range(y1-1, y2):
-            sum += arr[i][j]
+    for i in range(x1, x2+1):
+        if y1 != 0:
+            sum += (sum_arr[i][y2] - sum_arr[i][y1-1])
+        else:
+            sum += (sum_arr[i][y2])
 
-    print(f'sum={sum}')
+    result += f'{sum}\n'
+
+print(result)
