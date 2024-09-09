d4cc3a12773065afaea689f68580620c1d6cbcb0
jjunga1203 <jjunga1203@gmail.com>
2024-09-09 15:47:38 +0900

    프로그래머스 문제 연습 9/8

---
+++ programmers/176963.py
@@ -0,0 +1,22 @@
+def solution(name, yearning, photo):
+    answer = []
+    dict_list = {}
+
+    for i in range(len(name)):
+        dict_list[name[i]] = yearning[i]
+
+    for i in range(len(photo)):
+        sum = 0
+        for j in range(len(photo[i])):
+            if photo[i][j] in dict_list:
+                sum += dict_list[photo[i][j]]
+        answer.append(sum)
+    return answer
+
+
+name = ["kali", "mari", "don"]
+yearning = [11, 1, 55]
+photo = [["kali", "mari", "don"], ["pony", "tom", "teddy"], ["con", "mona", "don"]]
+# 결과 #[67, 0, 55]
+
+print(solution(name, yearning, photo))
